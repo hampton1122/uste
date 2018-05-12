a78893ab4bea30558c5d91990231d2fa6b03151d
Christopher Hampton <champton@Christophers-MacBook-Pro.local>
2018-05-12 09:13:59 -0400

    initial commit

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<app-header></app-header>
+  <app-intro></app-intro>
+  <app-services></app-services>
+  <app-testimonials [backgroundGray]="true"></app-testimonials>
+  <app-cta></app-cta>
+  <app-contact [backgroundGray]="true"></app-contact>
+  <app-footer></app-footer>`
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
